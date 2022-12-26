c1a63f7afa3b14c1c98d2ab0c7dadd73af2d8f12
utherpenpusher <lookathistuftedears@gmail.com>
2022-12-26 19:20:10 +0100

    add footstep controller and samples

--- Scripts/Sound/Controllers/FootstepController.cs
+++ Scripts/Sound/Controllers/FootstepController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class FootstepController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] List<AudioClip> clips = new List<AudioClip>();
+    [SerializeField, Range(0f, 1f)] float volumeMin = 0.8f;
+    [SerializeField, Range(0f, 1f)] float volumeMax = 1f;
+
+    GameObject player;
+
+    private void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayFootstep()
     {
-        
+        // TODO get SFX volume from settings and scale accordingly
+        int index = Random.Range(0, clips.Count);
+        float volume = Random.Range(volumeMin, volumeMax); // SCALE THIS
+        AudioSource.PlayClipAtPoint(clips[index], player.transform.position, volume);
     }
 }

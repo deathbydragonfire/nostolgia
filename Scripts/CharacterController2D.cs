8337b1d2f5d0b86d0fd7df186a5ffd5abc1fa004
Hanna Kessler <hanna.kessler10@gmail.com>
2022-12-28 09:11:04 -0600

    fixed jumping build issue

--- Scripts/CharacterController2D.cs
+++ Scripts/CharacterController2D.cs
@@ -134,7 +134,8 @@ public class CharacterController2D : MonoBehaviour
 		{
 			// Add a vertical force to the player.
 			m_Grounded = false;
-			m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+			Debug.Log(m_JumpForce);
+			m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce), ForceMode2D.Impulse);
 		}
 	}
 

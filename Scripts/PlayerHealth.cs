89ac4a03546562c496fe8d47fb21fe4c33653d82
Hanna Kessler <hanna.kessler10@gmail.com>
2022-12-31 15:54:20 -0600

    transition

--- Scripts/PlayerHealth.cs
+++ Scripts/PlayerHealth.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class PlayerHealth : MonoBehaviour
 {
     // Start is called before the first frame update
-    private int health = 1;
+    private int health = 8;
     private bool dead = false;
     float timer = 0f;
     public float deathtime = 1f;
